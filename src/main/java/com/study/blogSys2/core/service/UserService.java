f74d347082df8e65abd9e3aefbab2688595f45bf
s-chchen <1843093222@qq.com>
2021-08-02 19:57:25 +0800

    分层

---
+++ src/main/java/com/study/blogSys2/core/service/UserService.java
@@ -0,0 +1,52 @@
+package com.study.blogSys2.core.service;
+
+
+import com.study.blogSys2.core.entity.User;
+
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * @author 1034683568@qq.com
+ * @project_name ssm-maven
+ * @date 2017-3-1
+ */
+public interface UserService {
+
+    /**
+     * @param user
+     * @return
+     */
+    public User login(User user);
+
+    /**
+     * @param map
+     * @return
+     */
+    public List<User> findUser(Map<String, Object> map);
+
+    /**
+     * @param map
+     * @return
+     */
+    public Long getTotalUser(Map<String, Object> map);
+
+    /**
+     * @param user
+     * @return
+     */
+    public int updateUser(User user);
+
+    /**
+     * @param user
+     * @return
+     */
+    public int addUser(User user);
+
+    /**
+     * @param id
+     * @return
+     */
+    public int deleteUser(Integer id);
+}
